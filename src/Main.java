ebf7f0a2d778f26d3560ce1059b15e3fef905093
Mark Heijnekamp <26150@ma-web.nl>
2018-11-16 15:53:20 +0100

    more code

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,28 @@
 
 public class Main {
 
-    public static void main(String[] args) {
 
-functie();
+    public static void main(String[] args)
+    {
+        boolean bol = true;
+        int num = 10*10+100000000000-3/1523
+
+
+        if(bol){
+        functie();
+        bol = false;
+        }
+        if(!bol) {
+            functie2();
+            bol= true;
+        }
 
     }
-    public static void functie(){
+    public static void functie()
+    {
         System.out.println("IK BEN FUNCTIE");
+    }
+    public static void functie2(){
+        System.out.println("ik ben functie 2");
     }
 }
