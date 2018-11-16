3d223c48f9b0b44751b4c8511201980d7bd87fe2
Mark Heijnekamp <26150@ma-web.nl>
2018-11-16 16:02:58 +0100

    less code

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,9 @@ public class Main {
 
             functie2();
 
+            functie3("met DEPRESSIE");
+
+            functie4(19,28);
 
     }
     public static void functie()
@@ -18,5 +21,13 @@ public class Main {
     }
     public static void functie2(){
         System.out.println("ik ben functie 2");
+    }
+    public static void functie3(String text)
+    {
+        System.out.println("ik ben FUNCTIE 3 " + text);
+    }
+    public static void functie4(int num1, int num2){
+        System.out.println("ik ben duivel " + num1 +" "+ num2);
+
     }
 }
