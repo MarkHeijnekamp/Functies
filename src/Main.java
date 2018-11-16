87d346193690289a11255fdcdcbd51d98df91d59
Mark Heijnekamp <26150@ma-web.nl>
2018-11-16 15:46:54 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+
+functie();
+
+    }
+    public static void functie(){
+        System.out.println("IK BEN FUNCTIE");
+    }
+}
